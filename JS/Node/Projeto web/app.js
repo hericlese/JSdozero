1946492820caba224c51f34ecac7677d80e89553
unknown <hericlesaraujo@hotmail.com>
2020-04-06 17:08:37 -0300

    Logo Site Commit

--- JS/Node/Projeto web/app.js
+++ JS/Node/Projeto web/app.js
@@ -1,5 +1,7 @@
 const express =     require('express');
+const exphbs =      require('express-handlebars'); 
 const app =         express();
+const path=         require('path');
 const db =          require('./db/connection');
 const bodyParser =  require('body-parser');
 const PORT = 3000;
@@ -10,6 +12,12 @@ app.listen(PORT, function(){
 
 // apontando que irei utilizar body parser
 app.use(bodyParser.urlencoded({extended: false}));
+
+
+//handlebars    para compilação de views
+app.set('vews',path.join(__dirname,'views')); //diretorio das views
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));  // aquivo principal de layout da aplicação
+app.set('view engine', 'handlebars'); // dizendo qual a vew engine, framework que ira renderizar as views
 
 
 // db connections
